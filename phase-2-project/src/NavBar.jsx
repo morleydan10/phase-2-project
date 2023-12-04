cf01dceb30d0ea8201757785f1ac0159d826d354
Daniel Morley <morleydan10@gmail.com>
2023-12-04 15:11:12 -0500

    added NavBar Component/Page and css for Navabar

---
+++ phase-2-project/src/NavBar.jsx
@@ -0,0 +1,11 @@
+function NavBar() {
+    return (
+        <div>
+            <button className="navbar_button">Home</button>
+            <button className="navbar_button">ColorStudio</button>
+            <button className="navbar_button">My Pallets</button>
+        </div>
+    )
+}
+
+export default NavBar;
