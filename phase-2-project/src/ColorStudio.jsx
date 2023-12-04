54e87988ebebb02ba9b2c9dcb6763b3c0dfa814e
Daniel Morley <morleydan10@gmail.com>
2023-12-04 14:06:55 -0500

    added function and state to App page, added handlesubmit function and fetch to local json server to ColorStudio, added local json server with empty array for saved pallets

--- phase-2-project/src/ColorStudio.jsx
+++ phase-2-project/src/ColorStudio.jsx
@@ -1,11 +1,40 @@
 import { HexColorPicker } from "react-colorful";
 
-function ColorStudio() {
+function ColorStudio( {onAddPallet} ) {
     const [color, setColor] = useState("#aabbcc");
+
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        
+        
+        const newPallet = [{
+            primary: {},
+            secondary: {},
+            tertiary: {},
+            quarternary: {}
+        }
+    ]
+
+    fetch("http://localhost:3000/pallets")
+    .then((res) => res.json())
+    .then((data) => {
+        onAddPallet(data)
+    }), [] 
+};
+
 
     return (
         <div>
             <HexColorPicker color={color} onChange={setColor} />
+            <form>
+                onSubmit={handleSubmit}
+                {/* onChange and  */}
+                <input></input>
+                <input></input>
+                <input></input>
+                <input></input>
+            </form>
         </div>
     )
 }
