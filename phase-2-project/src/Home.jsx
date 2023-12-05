7e7db88fefff8d72baeeea630efc800aa9231e33
Daniel Morley <morleydan10@gmail.com>
2023-12-05 16:06:08 -0500

    addded button to ghome page

--- phase-2-project/src/Home.jsx
+++ phase-2-project/src/Home.jsx
@@ -5,6 +5,7 @@ function Home() {
             <div className="homepage-text">
                 <p>Welcome to the CSS Color Palette! Using our Color Studio, generate color palettes to use on all of your latest projects!</p>
                 <p>Here are some examples of what you can do using our website:</p>
+                <button className="get-started-button">Click here to get started</button>
             </div>
 
 
