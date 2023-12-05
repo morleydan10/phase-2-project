adbcacea987b7177c743c776553e4e0cc3f759ce
Daniel Morley <morleydan10@gmail.com>
2023-12-05 15:23:43 -0500

    fixed changes that did not commit

--- phase-2-project/src/ColorPallet.jsx
+++ phase-2-project/src/ColorPallet.jsx
@@ -53,19 +53,19 @@ isColorActive ? "colorActive" : "color"
     return(
         <>
         <div id="pallet-container">
-                <div className={cssClassActivate(selectedColor === "primary")} id="primary-color" >
+                <div className={selectedColor === "primary" ? "colorActive" : "color"} id="primary-color" >
                     <h2>Primary Color</h2>
                     <div id="primary" onClick={handleSelection} style={{backgroundColor: primary}}>x</div>
                 </div>
-                <div className={cssClassActivate(selectedColor === "secondary")} id="secondary-color">
+                <div className={selectedColor === "secondary" ? "colorActive" : "color"} id="secondary-color">
                     <h2>Secondary Color</h2>
                     <div id="secondary" onClick={handleSelection} style={{backgroundColor: secondary}}>x</div>
                 </div>
-                <div className={cssClassActivate(selectedColor === "tertiary")} id="tertiary-color" >
+                <div className={selectedColor === "tertiary" ? "colorActive" : "color"} id="tertiary-color" >
                     <h2>Tertiary Color</h2>
                     <div id="tertiary" onClick={handleSelection} style={{backgroundColor: tertiary}}>x</div>
                 </div>
-                <div className={cssClassActivate(selectedColor === "accent")} id="accent-color" >
+                <div className={selectedColor === "accent" ? "colorActive" : "color"} id="accent-color" >
                     <h2>Accent Color</h2>
                     <div id="accent" onClick={handleSelection} style={{backgroundColor: accent}}>x</div>
                 </div>
