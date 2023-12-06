0a09c57d814a58fee015ac4622f2047906711727
Daniel Morley <morleydan10@gmail.com>
2023-12-06 11:27:22 -0500

    edited json database, created get request in saved pallets to local database

--- phase-2-project/src/SavedColorPallets.jsx
+++ phase-2-project/src/SavedColorPallets.jsx
@@ -1,6 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function SavedColorPallets({ myPallets }) {
+
+    const [savedPallets, setSavedPallets] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/pallets")
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+        })
+    }, []
+    );
+
+
 
     return (
         <div>
