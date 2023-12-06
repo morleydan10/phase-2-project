68b13a6d40fe62ea7cf10c04441c509532b717c0
Daniel Morley <morleydan10@gmail.com>
2023-12-06 09:39:37 -0500

    add divs to homepage

--- phase-2-project/src/Home.jsx
+++ phase-2-project/src/Home.jsx
@@ -3,19 +3,25 @@ import { NavLink } from "react-router-dom";
 function Home() {
     return (
         <main>
-            <div className="homepage-div">
-                <h1 className="page-heading">Welcome!</h1>
-                <div className="homepage-text">
-                    <p>Welcome to the CSS Color Palette! Using our Color Studio, generate color palettes to use on all of your latest projects!</p>
-                    <p>Here are some examples of what you can do using our website:</p>
-                </div>
-                <div>
-                    <NavLink to="/ColorStudio">
-                        <button className="get-started-button">Click here to get started</button>
-                    </NavLink>
-                </div>
+            <div className="background-image-div">
+                <img id="background-image" src="https://m.media-amazon.com/images/W/MEDIAX_792452-T1/images/I/51CJu3t4PvL._AC_SL1024_.jpg">
+                    <div className="homepage-div">
+                        <h1 className="page-heading">Welcome!</h1>
+                        <div className="homepage-text">
+                            <p>Welcome to the CSS Color Palette! Using our Color Studio, generate color palettes to use on all of your latest projects!</p>
+                            <p>Here are some examples of what you can do using our website:</p>
+                            <div className="samples-div">
+                            {/* samples here */}
+                            </div>
+                        </div>
+                        <div>
+                            <NavLink to="/ColorStudio">
+                                <button className="get-started-button">Click    here to get started</button>
+                            </NavLink>
+                        </div>
+                    </div>
+                </img>
             </div>
-
 
         </main>
     )
