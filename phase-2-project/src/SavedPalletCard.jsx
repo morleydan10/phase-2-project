e06eac1d46d2e215bdb55ee3fa4530d347e0bea4
Daniel Morley <morleydan10@gmail.com>
2023-12-06 15:43:48 -0500

    css changes to saved pallets page, added buttons with navlink to colorstudio-can update url when path for extra page is created

--- phase-2-project/src/SavedPalletCard.jsx
+++ phase-2-project/src/SavedPalletCard.jsx
@@ -1,13 +1,24 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
-function SavedPalletCard({ primary, secondary, tertiary, quarternary, backgorund, titleFont, bodyFont}) {
+function SavedPalletCard({ id, primary, secondary, tertiary, quarternary, background, titleFont, bodyFont}) {
     return (
-        <div>
-            <h2 className="saved-pallet-heading">Name</h2>
-            <div className="card-primary" style={{backgroundColor: {primary}}}></div>
-            <div className="card-secondary" style={{backgroundColor: {secondary}}}></div>
-            <div className="card-tertiary" style={{backgroundColor: {tertiary}}}></div>
-            <div className="card-quarternary" style={{backgroundColor: {quarternary}}}></div>
+        <div className="saved-pallet-container">
+            {/* try and get css to load in rows instead of columns */}
+            <h2 className="saved-pallet-heading">Saved Palette {id}</h2>
+            <div className="card-color-div">
+                <div className="card-primary" style={{ backgroundColor: primary }}></div>
+                <div className="card-secondary" style={{ backgroundColor: secondary }}></div>
+                <div className="card-tertiary" style={{ backgroundColor: tertiary }}></div>
+                <div className="card-quarternary" style={{ backgroundColor: quarternary }}></div>
+            </div>
+            <div className="button-div">
+                {/* Need to change path to new load page */}
+                <NavLink  to="/ColorStudio">
+                    <button className="load-button">Load</button>
+                </NavLink>
+                <button className="delete-button">Delete</button>
+            </div>
         </div>
     )
 
