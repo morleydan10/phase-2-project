52295af15ddcb987a7319359e858d76e99b82afe
Daniel Morley <morleydan10@gmail.com>
2023-12-07 14:34:06 -0500

    css-changes

--- phase-2-project/src/Home.jsx
+++ phase-2-project/src/Home.jsx
@@ -8,7 +8,6 @@ function Home() {
                         <h1 className="page-heading">Welcome!</h1>
                         <div className="homepage-text">
                             <p>Welcome to the CSS Color Palette! Using our Color Studio, generate color palettes to use on all of your latest projects!</p>
-                            <p>Here are some examples of what you can do using our website:</p>
                             <div className="samples-div">
                             {/* samples here */}
                             </div>
